0e3b8b29d7bf9051487056a0d6fc97bff43dabbf
Josh Garde <joshgarde@gmail.com>
2022-08-05 14:55:06 -0700

    Initial project setup (#2)
    Co-authored-by: Joshua Garde <jgarde@jpl.nasa.gov>

---
+++ src/main/java/org/nasa/podaac/swodlr/SwodlrApp.java
@@ -0,0 +1,11 @@
+package org.nasa.podaac.swodlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwodlrApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SwodlrApp.class, args);
+    }
+}
