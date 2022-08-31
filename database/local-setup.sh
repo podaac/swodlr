e09251ea3ea12f4447ca3f875b13d737b201d92d
Joshua Garde <jgarde@jpl.nasa.gov>
2022-08-30 17:08:05 -0700

    Initial schema + local setup script

---
+++ database/local-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+DATABASE_NAME="swodlr"
+
+psql postgres $USER \
+    -a \
+    -v ON_ERROR_STOP=1 \
+    -c "DROP DATABASE IF EXISTS ${DATABASE_NAME};" \
+    -c "CREATE DATABASE ${DATABASE_NAME};" \
+    -c "\connect ${DATABASE_NAME}" \
+    -f schema.sql
--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,41 @@
--- NOP for now ;)
+-- Create tables
+CREATE TABLE "L2RasterProducts" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    "definition" uuid NOT NULL,
+    "currentStatus" uuid NOT NULL
+);
+
+CREATE TABLE "ProductHistory" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    "requestedBy" uuid NOT NULL,
+    "rasterProduct" uuid NOT NULL
+);
+
+CREATE TABLE "RasterDefinitions" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid()
+);
+
+CREATE TABLE "Status" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    "productID" uuid NOT NULL,
+    "previousStatus" uuid,
+    "timestamp" timestamp NOT NULL DEFAULT current_timestamp,
+    "status" text NOT NULL
+);
+
+CREATE TABLE "Users" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid()
+);
+
+-- Create references
+ALTER TABLE "L2RasterProducts"
+    ADD FOREIGN KEY ("definition") REFERENCES "RasterDefinitions" (id),
+    ADD FOREIGN KEY ("currentStatus") REFERENCES "Status" (id);
+
+ALTER TABLE "ProductHistory"
+    ADD FOREIGN KEY ("requestedBy") REFERENCES "Users" (id),
+    ADD FOREIGN KEY ("rasterProduct") REFERENCES "L2RasterProducts" (id);
+
+ALTER TABLE "Status"
+    ADD FOREIGN KEY ("productID") REFERENCES "L2RasterProducts" (id),
+    ADD FOREIGN KEY ("previousStatus") REFERENCES "Status" (id);
