7f7b32a5526ff7606ea5ba8ea92cde8d163dd5d7
Joshua Garde <jgarde@jpl.nasa.gov>
2022-09-07 16:10:34 -0700

    Cleanup import

--- src/main/java/gov/nasa/podaac/swodlr/status/Status.java
+++ src/main/java/gov/nasa/podaac/swodlr/status/Status.java
@@ -12,8 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Type;
-
 import gov.nasa.podaac.swodlr.l2_raster_product.L2RasterProduct;
 
 @Entity
