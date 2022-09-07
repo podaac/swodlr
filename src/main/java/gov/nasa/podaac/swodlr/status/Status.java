086b91c8856d852712a6a62ee2567f74a94af51b
Joshua Garde <jgarde@jpl.nasa.gov>
2022-09-06 20:56:29 -0700

    Cleanup imports

--- src/main/java/gov/nasa/podaac/swodlr/status/Status.java
+++ src/main/java/gov/nasa/podaac/swodlr/status/Status.java
@@ -5,8 +5,6 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
