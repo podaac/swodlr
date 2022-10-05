76f232b287f8067a74987891e3eb0ff6b53d695b
Joshua Garde <jgarde@jpl.nasa.gov>
2022-10-05 11:24:27 -0700

    Initial security config

---
+++ src/main/java/gov/nasa/podaac/swodlr/security/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package gov.nasa.podaac.swodlr.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    http
+      .cors().and()
+      .csrf().and()
+      .authorizeExchange(authorize -> {
+        authorize.anyExchange().authenticated();
+      })
+      .oauth2Client();
+
+    return http.build();
+  }
+}
