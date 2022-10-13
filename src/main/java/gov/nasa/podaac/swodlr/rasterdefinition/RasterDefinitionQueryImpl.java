ef2ae58a2fee08d60591abc5a604c1a13397ad02
Joshua Garde <jgarde@jpl.nasa.gov>
2022-10-13 12:55:29 -0700

    Add comment to RasterDefinitionQueryImpl

--- src/main/java/gov/nasa/podaac/swodlr/rasterdefinition/RasterDefinitionQueryImpl.java
+++ src/main/java/gov/nasa/podaac/swodlr/rasterdefinition/RasterDefinitionQueryImpl.java
@@ -15,6 +15,33 @@ public class RasterDefinitionQueryImpl implements RasterDefinitionQuery {
   @PersistenceContext
   private EntityManager entityManager;
 
+
+  /*
+   * This implementation is utilized to workaround an JPA issue with the
+   * PostgreSQL dialect in Hibernate where Hibernate does not parameterize
+   * null values as their original datatype and instead parameterizes as
+   * bytea's. This is a known issue of the PostgreSQL driver/dialect
+   * 
+   * This can lead to the exceptions:
+   *    - "ERROR: could not determine data type of parameter $1"
+   *    - "ERROR: operator does not exist: uuid = bytea"
+   * 
+   * Solutions researched include casting values, however, by casting
+   * in the query, we loose the benefits of parameterization in our
+   * prepared statements and create a statement which is harder to read.
+   * Workarounds such as COALESCE and testing other PostgreSQL dialects
+   * available in Hibernate were tried, but were found to either not
+   * solve the issue or still require the use of CASTs in queries
+   * 
+   * The hope is to one day remove this code in favor of Spring Data JPA
+   * queries, pending the PostgreSQL/Hibernate teams' cooperation with
+   * one another
+   * 
+   * Relevant discussions:
+   *    - https://stackoverflow.com/a/64223435
+   *    - https://stackoverflow.com/a/62680643
+   *    - https://github.com/pgjdbc/pgjdbc/issues/247#issuecomment-78213991
+   */
   @Override
   public List<RasterDefinition> findByParameter(
       UUID id,
