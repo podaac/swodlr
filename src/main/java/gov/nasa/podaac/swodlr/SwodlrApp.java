4115175f8161f3363da44303e73c38cb36712a2e
Joshua Garde <jgarde@jpl.nasa.gov>
2022-11-13 15:36:00 -0800

    SSM Parameter Store

--- src/main/java/gov/nasa/podaac/swodlr/SwodlrApp.java
+++ src/main/java/gov/nasa/podaac/swodlr/SwodlrApp.java
@@ -3,11 +3,17 @@ package gov.nasa.podaac.swodlr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class SwodlrApp {
   public static void main(String[] args) {
-    SpringApplication.run(SwodlrApp.class, args);
+    StandardEnvironment env = new StandardEnvironment();
+    env.getPropertySources().addFirst(new SsmProperties());
+
+    SpringApplication app = new SpringApplication(SwodlrApp.class);
+    app.setEnvironment(env);
+    app.run(args);
   }
 }
