265ae534b29bc1da2c33e8b4a99100c3fced7a7d
Joshua Garde <jgarde@jpl.nasa.gov>
2022-11-16 11:43:49 -0800

    Cleanup AWS SSM integration

--- src/main/java/gov/nasa/podaac/swodlr/SwodlrApp.java
+++ src/main/java/gov/nasa/podaac/swodlr/SwodlrApp.java
@@ -3,17 +3,11 @@ package gov.nasa.podaac.swodlr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class SwodlrApp {
   public static void main(String[] args) {
-    StandardEnvironment env = new StandardEnvironment();
-    env.getPropertySources().addFirst(new SsmProperties());
-
-    SpringApplication app = new SpringApplication(SwodlrApp.class);
-    app.setEnvironment(env);
-    app.run(args);
+    SpringApplication.run(SwodlrApp.class, args);
   }
 }
