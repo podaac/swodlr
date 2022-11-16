512f2b1bb531b0b2cb22486fbd472deb40a79474
Joshua Garde <jgarde@jpl.nasa.gov>
2022-11-16 12:56:32 -0800

    Restrict ecs-task-policy SSM permissions

--- terraform/app.tf
+++ terraform/app.tf
@@ -189,7 +189,7 @@ resource "aws_iam_role" "app_task" {
             "ssm:GetParametersByPath"
           ]
           Effect = "Allow"
-          Resource = "*"
+          Resource = "arn:aws:ssm:${var.region}:${local.account_id}:parameter/${local.app_path}/*"
         }
       ]
     })
