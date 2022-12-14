53de0e306fa1831aa93dab65acf6e117e26d940e
Joshua Garde <jgarde@jpl.nasa.gov>
2022-12-14 15:52:19 -0800

    Fix lazy user ref loading

--- src/main/java/gov/nasa/podaac/swodlr/security/GraphQlUserInjector.java
+++ src/main/java/gov/nasa/podaac/swodlr/security/GraphQlUserInjector.java
@@ -1,9 +1,7 @@
 package gov.nasa.podaac.swodlr.security;
 
-import gov.nasa.podaac.swodlr.user.User;
 import gov.nasa.podaac.swodlr.user.UserReference;
 import java.util.Collections;
-import java.util.Optional;
 import org.springframework.graphql.server.WebGraphQlInterceptor;
 import org.springframework.graphql.server.WebGraphQlRequest;
 import org.springframework.graphql.server.WebGraphQlResponse;
@@ -19,14 +17,9 @@ public class GraphQlUserInjector implements WebGraphQlInterceptor {
       if (context.hasKey(ServerWebExchange.class)) {
         return context.get(ServerWebExchange.class).getSession().flatMap((session) -> {
           UserReference userReference = session.getAttribute("user");
-          if (userReference == null) {
-            return chain.next(request);
-          }
-
-          Optional<User> result = userReference.fetch();
-          if (result.isPresent()) {
+          if (userReference != null) {
             request.configureExecutionInput((executionInput, builder) -> {
-              builder.graphQLContext(Collections.singletonMap("user", result.get()));
+              builder.graphQLContext(Collections.singletonMap("user", userReference));
               return builder.build();
             });
           }
