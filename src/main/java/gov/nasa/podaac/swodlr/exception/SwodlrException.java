4003c69753b7b3b3bbb404153e7606976cb01d4f
Joshua Garde <jgarde@jpl.nasa.gov>
2023-01-10 03:00:32 -0800

    SwodlrException documentation

--- src/main/java/gov/nasa/podaac/swodlr/exception/SwodlrException.java
+++ src/main/java/gov/nasa/podaac/swodlr/exception/SwodlrException.java
@@ -1,5 +1,10 @@
 package gov.nasa.podaac.swodlr.exception;
 
+/*
+ * A SwodlrException is a RuntimeException that is safe to display to users.
+ * SwodlrExceptions should not contain any internal system state/information and
+ * inform the user on how they should further proceed
+ */
 public class SwodlrException extends RuntimeException {
   public SwodlrException(String message) {
     super(message);
