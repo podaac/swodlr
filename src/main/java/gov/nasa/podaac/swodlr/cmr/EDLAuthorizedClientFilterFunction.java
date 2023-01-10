8bad886b84c897023ae9b147cbe0daf4118ec801
Joshua Garde <jgarde@jpl.nasa.gov>
2023-01-10 03:02:39 -0800

    Authentication fixes

--- src/main/java/gov/nasa/podaac/swodlr/cmr/EDLAuthorizedClientFilterFunction.java
+++ src/main/java/gov/nasa/podaac/swodlr/cmr/EDLAuthorizedClientFilterFunction.java
@@ -2,10 +2,13 @@ package gov.nasa.podaac.swodlr.cmr;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
@@ -15,10 +18,12 @@ import reactor.core.publisher.Mono;
 public class EDLAuthorizedClientFilterFunction implements ExchangeFilterFunction {
   private static final String CLIENT_REGISTRATION_ID = "edl";
 
-  private ReactiveOAuth2AuthorizedClientService authorizedClientService;
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
-  public EDLAuthorizedClientFilterFunction(ReactiveOAuth2AuthorizedClientService authorizedClientService) {
-    this.authorizedClientService = authorizedClientService;
+  private ReactiveOAuth2AuthorizedClientManager authorizedClientManager;
+
+  public EDLAuthorizedClientFilterFunction(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+    this.authorizedClientManager = authorizedClientManager;
   }
 
   @Override
@@ -31,13 +36,20 @@ public class EDLAuthorizedClientFilterFunction implements ExchangeFilterFunction
             .build();
 
           return next.exchange(newRequest);
-    });
+        })
+        .switchIfEmpty(Mono.defer(() -> {
+          logger.error("Authorized client not found");
+          return Mono.empty();
+        }));
   }
 
   private Mono<OAuth2AuthorizedClient> getAuthorizedClient(SecurityContext context) {
     return Mono.defer(() -> {
-      String principalName = context.getAuthentication().getName();
-      return authorizedClientService.loadAuthorizedClient(CLIENT_REGISTRATION_ID, principalName);
+      OAuth2AuthorizeRequest request = OAuth2AuthorizeRequest
+          .withClientRegistrationId(CLIENT_REGISTRATION_ID)
+          .principal(context.getAuthentication())
+          .build();
+      return authorizedClientManager.authorize(request);
     });
   }
 }
